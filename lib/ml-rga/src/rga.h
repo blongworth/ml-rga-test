fe24e3afca63cfcca1a0863c7393fda009bd0c42
Brett Longworth <blongworth@whoi.edu>
2023-11-11 12:11:18 -0500

    leave packetLength unset initially.

--- lib/ml-rga/src/rga.h
+++ lib/ml-rga/src/rga.h
@@ -50,7 +50,7 @@ private:
     float emissionCurrentInc = 0.1;
     static const bool cdemPresent = true;
     bool newData;
-    byte packetLength = 4;
+    byte packetLength;
     byte packet[MAX_PACKET_LENGTH];
     void readSerial();
     void flushReadBuffer();
